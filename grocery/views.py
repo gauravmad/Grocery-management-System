040eaec01bbc0135ae8cb09028d8fadb85ad429e
Gaurav Madan <gauravmadan2004@gmail.com>
2023-02-23 02:08:12 +0530

    first commit

---
+++ grocery/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+
+def SignUpPage(request):
+    status=''
+    status1=''
+    if request.method == "POST":
+        uname= request.POST.get('username')
+        email= request.POST.get('email')
+        pass1= request.POST.get('password1')
+        pass2= request.POST.get('password2')
+
+        status1="Password doesn't match!!"
+        if pass1!=pass2:
+            return render(request,"signup.html",{'status1':status1})
+        else:
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            
+        status="Your Account has been created successfully! &#128512;"
+    return render(request,"signup.html",{'status':status})
+
+def LoginPage(request):
+    status2=''
+    if request.method == "POST":
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username, password=pass1)
+
+        status2="Username or Password is incorrect"
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return render(request,"login.html",{'status2':status2})
+
+    return render(request,"login.html")
+
+def home(request):
+    return render(request,"home.html")
