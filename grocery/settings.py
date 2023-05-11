cfde561d36cfb4cf1b455ef731afe8b77d454de1
Gaurav Madan <gauravmadan2004@gmail.com>
2023-05-12 01:08:03 +0530

    commit

--- grocery/settings.py
+++ grocery/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mc#@wo*(uz9x1rl@e)^y0w52#7gl0dv=s(**ep(=-^uch0eq4c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [ 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- grocery/urls.py
+++ grocery/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 from grocery import views
 
 from groceryproducts.views import GroceryList, GroceryDetail
@@ -34,7 +37,10 @@ urlpatterns = [
     path('cart/', views.cart, name="cart" ),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name="add_to_cart" ),
     path('groceries/<int:pk>/', GroceryDetail.as_view(), name='grocery-detail'),
-    path('payment/',views.payment,name='payment')
+    path('payment/',views.payment,name='payment'),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
